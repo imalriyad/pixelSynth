ed9cb9be2616ef3195a2c229a9815496bed0eb77
AL Riyad <imalriyad@gmail.com>
2024-03-28 22:28:17 +0600

    initial setup and header section with input feild and select method done

---
+++ src/components/pages/Header.jsx
@@ -0,0 +1,55 @@
+const Header = () => {
+  return (
+    <>
+      <div
+        className="hero min-h-[60vh]"
+        style={{
+          backgroundImage: "url(https://i.ibb.co/6tkKxN2/herobg.png)",
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="">
+            <h1 className="mb-5 md:text-4xl xl:text-5xl text-xl font-bold text-white">
+              An AI Image Generation Transforms Digital Art || Revolutionize
+              Creativity
+            </h1>
+            <p className="md:mb-12 mb-5 max-w-2xl mx-auto md:text-sm xl:text-base text-xs">
+              Discover boundless creativity with our AI image generation tool.
+              Instantly produce stunning visuals tailored to your needs using
+              advanced machine learning algorithms
+            </p>
+            <div className="relative  mx-auto max-w-2xl">
+              {/* Input field for getting prompt */}
+              <input
+                type="text"
+                placeholder="Describe what you want generate . . ."
+                className="input rounded-full md:input-md input-sm md:placeholder:text-sm placeholder:text-xs pl-4 md:pl-6 text-black w-full "
+              />
+
+              <span className="md:absolute top-0 right-0 flex flex-col">
+                {/* Select image quantity */}
+                <select className="select-xs md:select-md md:rounded-none rounded-full select md:absolute select-bordered md:right-24 md:mt-0 mt-1 md:mb-0 mb-4  focus:outline-none text-black ">
+                  <option value={6}>6 Images</option>
+                  <option value={5}>5 Images</option>
+                  <option value={4} selected>
+                    4 Images
+                  </option>
+                  <option value={3}>3 Images</option>
+                  <option value={2}>2 Images</option>
+                </select>
+
+                {/* Genarte button */}
+                <button className="btn md:btn-md btn-sm md:absolute right-0 btn-primary md:rounded-r-full rounded-full md:rounded-none">
+                  Generate
+                </button>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
